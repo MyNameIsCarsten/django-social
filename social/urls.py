287da2b8edb7a0e22f8e120925a94d46e02e7359
Carsten Linder <Carsten@Carsten-Linder.de>
2023-08-30 14:55:35 +0200

    Completed Part 2

--- social/urls.py
+++ social/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # redirect all requests made to the base URL ("") to your dwitter app’s urls.py file
+    path("", include("dwitter.urls")),
     path('admin/', admin.site.urls),
 ]
