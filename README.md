3632c1ddcc4cbc4136dbae1d90fca628c7c7991a
Carsten Linder <Carsten@Carsten-Linder.de>
2023-08-30 13:59:38 +0200

    Finished Part 1

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # django-social
 
 [Tutorial Part 1](https://realpython.com/django-social-network-1/)
-- [ x ] Step 1: Set Up the Base Project
+- [ x ] [Step 1: Set Up the Base Project](https://realpython.com/django-social-network-1/#step-1-set-up-the-base-project)
     - [ x ] Create a virtual environment and install Django
     - [ x ] Create a Django project and app
     - [ x ] Customize the Django admin interface
     - [ x ] Create users for your app
-- [  ] Step 2: Extend the Django User Model
-- [  ] Step 3: Implement a Post-Save Hook
+- [  ] [Step 2: Extend the Django User Model](https://realpython.com/django-social-network-1/#step-2-extend-the-django-user-model)
+    - [ x ] Create a Profile Model
+    - [ x ] Display Profile Information Within the User Admin Page
+- [  ] [Step 3: Implement a Post-Save Hook](https://realpython.com/django-social-network-1/#step-3-implement-a-post-save-hook)
+    - [ x ] Coordinate Users and Profiles With a Signal
+    - [ x ] Add Functionality Through Error-Driven Development
+    - [ x ] Refactor Your Code Using a Decorator
+    - [ x ] Confirm the Automatic Association in Your Admin
--- dwitter/models.py
+++ dwitter/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+# import the built-in User model that we want to extend
+from django.contrib.auth.models import User
+
+# import post_save
+from django.db.models.signals import post_save
+
+# import receiver
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    # define a OneToOneField object called user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # define a ManyToManyField object with the field name follows
+    follows = models.ManyToManyField(
+        "self",
+        # access data entries from the other end of the relationship
+        related_name="followed_by",
+        # users can follow someone without them following back
+        symmetrical=False,
+        # users don’t need to follow anyone
+        blank=True
+    )
+
+    # overloaded the default .__str__() method so that it 
+    # returns the value of username from the associated instance of the User model
+    def __str__(self):
+        return self.user.username
+ 
+# apply the receiver decorator to create_profile
+# passing it post_save and passing the User model to sender
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    # ceated is provided by post_save
+    if created:
+        # associate a profile with a user
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # add the profile associated with your new user account to the list of accounts that the user is following
+        user_profile.follows.set([instance.profile.id])
+        # commit the new profile to your database
+        user_profile.save()
+
